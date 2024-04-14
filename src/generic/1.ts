08344334c91550d464777d861c3fff3b638f38d4
NataOz <merlincat.no@gmail.com>
2024-04-14 16:22:04 +0300

    bugs fix, task 1, generic, task 4 generic

--- src/generic/1.ts
+++ src/generic/1.ts
@@ -3,8 +3,9 @@
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
 
-function getPromise<T extends (string | number)[]>() {
-  return new Promise<T>((resolve) => {
+function getPromise<T extends (string | number)[]>(): Promise<T> {
+  //забула за тип повернення, дякую)
+  return new Promise((resolve) => {
     resolve(["Text", 50] as T);
   });
 }
